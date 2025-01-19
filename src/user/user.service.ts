45b7e6a5ed60ba44ae949d6e69041f89c71a8074
arufonsekun <junior.ramisch@gmail.com>
2025-01-19 14:30:03 -0300

    chore(UserService): adds store and findOneByEmail (no tests yet)

---
+++ src/user/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, Inject } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { CreateUserDto, User, UserModel } from './user.schema';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject('USER_MODEL')
+    private userModel: Model<User>,
+  ) {}
+
+  async store(userToCreate: CreateUserDto): Promise<User> {
+    const password = await bcrypt.hash(userToCreate.password, 10);
+    const user = new this.userModel({ ...userToCreate, password });
+    return user.save();
+  }
+
+  async findOneByEmail(email: string): Promise<any | undefined> {
+    return await UserModel.findOne({ email }).exec();
+  }
+}
