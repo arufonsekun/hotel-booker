524504cc969dff01e8bdb584819facac2d37b474
arufonsekun <junior.ramisch@gmail.com>
2025-01-19 14:34:11 -0300

    chore: adds database provider for user create

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,15 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User, UserSchema } from './user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { userProviders } from './user.provider';
+import { DatabaseModule } from 'src/database/database.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    DatabaseModule,
   ],
-  providers: [UserService],
+  providers: [UserService, ...userProviders],
   exports: [UserService],
   controllers: [UserController],
 })
